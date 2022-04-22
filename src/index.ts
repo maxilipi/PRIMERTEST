94becb3e17b29ee86663f7a6359e450df5fa2012
maxilipi <104212047+maxilipi@users.noreply.github.com>
2022-04-22 12:17:06 -0300

    agregue una carita feliz

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-console.log("que capo que soy, ya estoy usando github y voy a hacer mi primer commit");
+console.log(
+  "que capo que soy, ya estoy usando github y voy a hacer mi primer commit  :)"
+);
